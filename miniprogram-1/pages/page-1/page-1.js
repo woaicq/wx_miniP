030221f565b6db23f2417859c0420b03588f44c8
woaicq <tourme@sohu.com>
2020-02-20 15:35:46 +0800

    my 1st version
    P1-P6

---
+++ miniprogram-1/pages/page-1/page-1.js
@@ -0,0 +1,82 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    pri_button_name: "属性是高亮绿色的按钮的名字by data",
+    below_button_text: "“a text”",
+    mysum: 0,
+    itemx:[""],
+    news:["a","b","c"],
+    foot1_str:"foot1 in page",
+    myheader:"h in p"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log("onLoad")
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    console.log("onReady")
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log("onShow")
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    console.log("onHide")
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    console.log("onUnload")
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    console.log("onPullDownRefresh")
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    console.log("onReachBottom")
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    console.log("onShareAppMessage")
+  },
+
+  /*
+  一个简单的按钮相应事件
+  */
+ btnClick: function(){
+   var a = this.data.mysum;
+   var alhpabeta = this.data.news;
+   alhpabeta.shift();
+   this.setData({below_button_text: "mysum", mysum: a+1, news: alhpabeta});
+  console.log(this.data.news)
+ }
+})
